dedf6909e0e7c96e0f5f246fcbfdaa38ebef7434
SOPHIA DIPAOLA <sophiadipaola@SOPHIAs-MacBook-Pro.local>
2021-01-27 15:13:24 -0800

    working on game page functionality

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+// GAME JS
+//variable list
+// const rightAnswer = document.querySelectorAll('.correct-answer')
+// const wrongAnswer = document.querySelectorAll('.incorrect-answer')
+// let score = 0
+// const next = document.querySelector('.score')
+// const firstQ = document.querySelector('.first')
+// const secondQ = document.querySelector('.second')
+// const thirdQ = document.querySelector('.third')
+// const fourthQ =document.querySelector('.fourth')
+// const fifthQ = document.querySelector('.fifth')
+// const next = document.getElementById('next-question')
+
+let Q1s = document.querySelectorAll('.Q1')
+console.log(Q1s)
+for(oneQ of Q1s){
+    oneQ.disabled = true
+    console.log(oneQ)
+}
+       
+
+let Q2s = document.querySelectorAll('.Q2')
+console.log(Q2s)
+for(twoQ of Q2s){
+    twoQ.disabled = true
+    console.log(twoQ)
+}
+
+let Q3s = document.querySelectorAll('.Q3')
+console.log(Q3s)
+for(threeQ of Q3s){
+    threeQ.disabled = true
+    console.log(threeQ)
+}
+
+let Q4s = document.querySelectorAll('.Q4')
+console.log(Q4s)
+for(fourQ of Q4s){
+    fourQ.disabled = true
+    fourconsole.log(Q)
+}
+
+let Q5s = document.querySelectorAll('.Q5')
+console.log(Q5s)
+for(fiveQ of Q5s){
+    fiveQ.disabled = true
+    console.log(fiveQ)
+}
+
+
+
+
+
 // const questions = [
 //   {
 //     title: "Who is jimbo?",
